94c857832a8f82a00b98bf1d30eeb88c5c1e3784
djh06c <djdavidjorgen@gmail.com>
2025-04-24 13:11:45 +0200

    ye

--- src/main/java/com/example/carrenting_2_electric_boogaloo/CarRenting2ElectricBoogalooApplication.java
+++ src/main/java/com/example/carrenting_2_electric_boogaloo/CarRenting2ElectricBoogalooApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarRenting2ElectricBoogalooApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CarRenting2ElectricBoogalooApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CarRenting2ElectricBoogalooApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/carrenting_2_electric_boogaloo/Controller/CarController.java
+++ src/main/java/com/example/carrenting_2_electric_boogaloo/Controller/CarController.java
@@ -1,4 +1,14 @@
-package Controller;
+package com.example.carrenting_2_electric_boogaloo.Controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class CarController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
 }
