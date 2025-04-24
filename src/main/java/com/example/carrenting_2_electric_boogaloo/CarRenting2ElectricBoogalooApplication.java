f976cbd21ef0c3abc35703ed9f32bf7641becd55
djh06c <djdavidjorgen@gmail.com>
2025-04-24 12:57:46 +0200

    first commit

---
+++ src/main/java/com/example/carrenting_2_electric_boogaloo/CarRenting2ElectricBoogalooApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrenting_2_electric_boogaloo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRenting2ElectricBoogalooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRenting2ElectricBoogalooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/carrenting_2_electric_boogaloo/CarRenting2ElectricBoogalooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carrenting_2_electric_boogaloo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRenting2ElectricBoogalooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
