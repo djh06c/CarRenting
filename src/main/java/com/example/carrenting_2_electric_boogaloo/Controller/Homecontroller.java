dab11cd4dc83dcdc992660be0548073635addcb3
djh06c <djdavidjorgen@gmail.com>
2025-04-24 14:06:02 +0200

    yehh

---
+++ src/main/java/com/example/carrenting_2_electric_boogaloo/Controller/Homecontroller.java
@@ -0,0 +1,13 @@
+package com.example.carrenting_2_electric_boogaloo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Homecontroller {
+
+    @GetMapping("/")
+    public String home() {
+        return "home/index";
+    }
+}
